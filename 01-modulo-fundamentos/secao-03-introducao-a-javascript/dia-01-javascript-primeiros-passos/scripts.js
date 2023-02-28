365e30bc4683ce8b91c4711bfb4c673c6ed53bec
LeticiaBastos <silviabastoskika@hotmail.com>
2023-02-28 15:01:50 -0300

    continua exercicios

--- 01-modulo-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ 01-modulo-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -6,6 +6,17 @@ let perimeter = 5 +5 + 8 + 8;
 console.log(area);
 console.log(perimeter);
 
+let nota = 90;
+
+if (nota >= 80) {
+    console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+} else if (nota < 80 && nota >= 60) {
+    console.log ('Você está na nossa lista de espera.');
+} else if (nota < 60) {
+    console.log ('Infelizmente, você reprovou');
+}
+
+
 let a = 43;
 let b = 65;
 
@@ -28,3 +39,4 @@ else if (a < b) {
 else {
     console.log('Tudo bem, idade não importa mesmo!');
 }
+
