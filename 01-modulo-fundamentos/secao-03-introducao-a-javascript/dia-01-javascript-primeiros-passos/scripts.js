cea3bc65cf5fabb6e74fca0a6d3762edf92d0cfc
LeticiaBastos <silviabastoskika@hotmail.com>
2023-02-27 23:26:21 -0300

    continua exercicios

--- 01-modulo-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ 01-modulo-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -10,3 +10,13 @@ console.log(a * b);
 console.log(a / b);
 
 console.log(a % b);
+
+if (a > b) {
+   console.log('Eu sou mais velha');     
+}
+else if (a < b) {
+    console.log('O Julio ê mais velho');
+}
+else {
+    console.log('Tudo bem, idade não importa mesmo!');
+}
